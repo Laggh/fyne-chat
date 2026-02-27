4a0c05320b7d05308ca6322a1a2512a066fe6566
Renan Andrade <77841497+Laggh@users.noreply.github.com>
2026-02-27 20:29:19 -0300

    Update index.go

--- api/index.go
+++ api/index.go
@@ -5,42 +5,16 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"sync"
 
 	"github.com/go-redis/redis/v8"
 )
 
-var Ctx = context.Background()
-var client *redis.Client
-var once sync.Once
-
-func GetRedis() (*redis.Client, error) {
-	var err error
-	once.Do(func() {
-		redisURL := os.Getenv("REDIS_URL")
-		if redisURL == "" {
-			err = fmt.Errorf("REDIS_URL vazio")
-			return
-		}
-
-		opt, parseErr := redis.ParseURL(redisURL)
-		if parseErr != nil {
-			err = fmt.Errorf("ParseURL falhou: %v", parseErr)
-			return
-		}
-
-		client = redis.NewClient(opt)
-	})
-	return client, err
-}
+var ctx = context.Background()
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	c, err := GetRedis()
-	if err != nil {
-		fmt.Fprintf(w, "Redis error: %v", err)
-		return
-	}
+	opt, _ := redis.ParseURL(os.Getenv("REDIS_URL"))
+	client := redis.NewClient(opt)
 
-	val := c.Get(Ctx, "foo").Val()
+	val := client.Get(ctx, "foo").Val()
 	fmt.Fprintf(w, "<h1>Hello!</h1><p>%s</p>", val)
 }
