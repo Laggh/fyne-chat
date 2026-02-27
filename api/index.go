972d0ea02bb53360cafacc41a91367cf52752b33
Renan Andrade <77841497+Laggh@users.noreply.github.com>
2026-02-27 20:25:30 -0300

    a

--- api/index.go
+++ api/index.go
@@ -1,10 +1,46 @@
 package handler
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"sync"
+
+	"github.com/go-redis/redis/v8"
 )
 
+var Ctx = context.Background()
+var client *redis.Client
+var once sync.Once
+
+func GetRedis() (*redis.Client, error) {
+	var err error
+	once.Do(func() {
+		redisURL := os.Getenv("REDIS_URL")
+		if redisURL == "" {
+			err = fmt.Errorf("REDIS_URL vazio")
+			return
+		}
+
+		opt, parseErr := redis.ParseURL(redisURL)
+		if parseErr != nil {
+			err = fmt.Errorf("ParseURL falhou: %v", parseErr)
+			return
+		}
+
+		client = redis.NewClient(opt)
+	})
+	return client, err
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	c, err := GetRedis()
+	if err != nil {
+		fmt.Fprintf(w, "Redis error: %v", err)
+		return
+	}
+
+	val := c.Get(Ctx, "foo").Val()
+	fmt.Fprintf(w, "<h1>Hello!</h1><p>%s</p>", val)
 }
--- go.mod
+++ go.mod
@@ -7,7 +7,9 @@ require fyne.io/fyne/v2 v2.7.3
 require (
 	fyne.io/systray v1.12.0 // indirect
 	github.com/BurntSushi/toml v1.5.0 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fredbi/uri v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
 	github.com/fyne-io/gl-js v0.2.0 // indirect
@@ -16,6 +18,7 @@ require (
 	github.com/fyne-io/oksvg v0.2.0 // indirect
 	github.com/go-gl/gl v0.0.0-20231021071112-07e5d0ea2e71 // indirect
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20240506104042-037f3cc74f2a // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/go-text/render v0.2.0 // indirect
 	github.com/go-text/typesetting v0.3.3 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
